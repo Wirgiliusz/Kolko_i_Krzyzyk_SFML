5c7bba6fa162a28a5dcd775d7e63bffc6dcbcec4
Wirgiliusz <kamx98@gmail.com>
2019-05-15 12:19:11 +0200

    Dzialajacy main loop

--- main.cpp
+++ main.cpp
@@ -6,9 +6,25 @@ using namespace std;
 
 
 int main() {
-    GameMaster GM;  
+    GameMaster GM;
+    char input;
+    bool wykonanoRuch = false;
     GM.plansza = GM.stworzPlansze(5,3);
+    GM.plansza->wyswietlPlansze();
 
+    // --- Glowna petla gry --- //
+    while(true) {
+        cin >> input;
+        while(wykonanoRuch != true) {
+            wykonanoRuch = GM.ruch(input);
+        }
+        cout << "Wygrana: " << GM.plansza->wygrana() << endl;
+
+        GM.plansza->wyswietlPlansze();
+        GM.nastepnaTura();
+        wykonanoRuch = false;
+    }
+    /*
     GM.plansza->wyswietlPlansze();
 
     //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[0][0]);
@@ -45,11 +61,11 @@ int main() {
     GM.ruch('r');
     cout << "Wygrana: " << GM.plansza->wygrana() << endl;
     GM.plansza->wyswietlPlansze();
+    */
 
     cout << rang::style::reset << rang::fg::reset << rang::bg::reset;
     system("pause");
     return 0;
-
 }
 
 /*
