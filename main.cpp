a6da8f5baa67de9134f67b764c061c0ce5460816
Wirgiliusz <kamx98@gmail.com>
2019-05-15 12:20:34 +0200

    oczyszczenie maina

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
     GM.plansza->wyswietlPlansze();
 
     // --- Glowna petla gry --- //
-    while(true) {
+    while(!GM.plansza->wygrana()) {
         cin >> input;
         while(wykonanoRuch != true) {
             wykonanoRuch = GM.ruch(input);
@@ -24,44 +24,7 @@ int main() {
         GM.nastepnaTura();
         wykonanoRuch = false;
     }
-    /*
-    GM.plansza->wyswietlPlansze();
 
-    //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[0][0]);
-    GM.ruch('a');
-    cout << "Wygrana: " << GM.plansza->wygrana() << endl;
-    GM.plansza->wyswietlPlansze();
-    GM.nastepnaTura();
-
-    //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[1][2]);
-    GM.ruch('q');
-    cout << "Wygrana: " << GM.plansza->wygrana() << endl;
-    GM.plansza->wyswietlPlansze();
-    GM.nastepnaTura();
-
-    //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[3][3]);
-    GM.ruch('n');
-    cout << "Wygrana: " << GM.plansza->wygrana() << endl;
-    GM.plansza->wyswietlPlansze();
-    GM.nastepnaTura();
-
-    //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[2][1]);
-    GM.ruch('s');
-    cout << "Wygrana: " << GM.plansza->wygrana() << endl;
-    GM.plansza->wyswietlPlansze();
-    GM.nastepnaTura();
-
-    //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[0][2]);
-    GM.ruch('x');
-    cout << "Wygrana: " << GM.plansza->wygrana() << endl;
-    GM.plansza->wyswietlPlansze();
-    GM.nastepnaTura();
-
-    //GM.aktualnyGracz->wykonajRuch(GM.plansza->macierzPol[0][2]);
-    GM.ruch('r');
-    cout << "Wygrana: " << GM.plansza->wygrana() << endl;
-    GM.plansza->wyswietlPlansze();
-    */
 
     cout << rang::style::reset << rang::fg::reset << rang::bg::reset;
     system("pause");
